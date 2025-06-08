c6658fe97af76e649540613eca2e2e37eadb94c3
Souvik <svkroy41995@outlook.com>
2025-06-08 23:10:03 +0530

    main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from function.chunker import chunk_text
+from function.ingest import parse_document
+from fastapi import UploadFile, File
+from typing import List
+from pathlib import Path
 import uvicorn
 
 app = FastAPI()
@@ -16,6 +21,19 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/api/ingestchunk')
+async def ingest_chunk(file: UploadFile = File(...)):
+    path = f"docs/{file.filename}"
+    with open(path, "wb") as buffer:
+        content = await file.read()
+        buffer.write(content)
+    
+    text = parse_document(path)
+    chunks = chunk_text(text)
+
+    return {"chunks": chunks[:3]}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
